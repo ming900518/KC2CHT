e56d902e2b8d8b3bd75ed5afaaa9677df5201f30
Ming Chang <ming900518@gmail.com>
2020-09-19 22:13:22 +0800

    Release 2.6 Update 1
    1. Modify some build setting to let this app be able to use Xcode 12 to compile.
    2. Update CocoaPod

--- Pods/Toaster/Sources/ToastView.swift
+++ Pods/Toaster/Sources/ToastView.swift
@@ -47,6 +47,8 @@ open class ToastView: UIView {
     case .pad: return 60
     case .tv: return 90
     case .carPlay: return 30
+    case .mac: return 30
+    @unknown default: return 30
     }
   }()
 
@@ -58,6 +60,8 @@ open class ToastView: UIView {
     case .pad: return 40
     case .tv: return 60
     case .carPlay: return 20
+    case .mac: return 20
+    @unknown default: return 20
     }
   }()
 
@@ -82,6 +86,8 @@ open class ToastView: UIView {
       case .pad: return .systemFont(ofSize: 16)
       case .tv: return .systemFont(ofSize: 20)
       case .carPlay: return .systemFont(ofSize: 12)
+      case .mac: return .systemFont(ofSize: 12)
+      @unknown default: return .systemFont(ofSize: 12)
       }
     }()
     self.numberOfLines = 0
